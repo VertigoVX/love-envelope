6d28ef6f53f2b96a093eabdc4a70ba5832c05357
Tristan David <107416696+VertigoVX@users.noreply.github.com>
2025-04-05 00:58:24 +0200

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -4,7 +4,9 @@ const messageDatabase = {
                     "I miss you soooo much babygirl, I can't wait to have you in my arms again.",
                     "I miss you too my love, you're my favourite part of everyday, I can't wait to spend forever with you.",
                     "Heyyy my super sexy super beautiful creampuff of a woman, you better be missing me 'cause imma smother you with kisses when I see you.",
-                    "I'm currently sending you a virtual hug so if you feel a warm presence around you just know it's me. (i'm also squeezing your butt)."
+                    "I'm currently sending you a virtual hug so if you feel a warm presence around you just know it's me. (i'm also squeezing your butt).",
+                    "Since we met I don't think there's been a day where I haven't missed you my love.",
+                    "I miss you so much babe I can't wait until we meet again!!!!!!"
           ],
           laugh: [
                     "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them! 😂",
@@ -12,17 +14,22 @@ const messageDatabase = {
                     "Remember the time I kissed you and you ran away? that was a funny time.",
                     "Remember the time you got a cramp during sexy time? hehehe.",
                     "What do you call a fake noodle? An impasta! 🍝",
+                    "Have you heard about that shady character who claims he can make deer run faster? He's trying to make a quick Buck.",
+                    "How would you cancel an appointment at a sperm bank? just call and say you can't come"
 
           ],
           comfort: [
                     "You are stronger than you know, braver than you believe, and more loved than you can imagine.",
                     "Whatever you're going through, remember that I'm here for you. Always.",
                     "It's okay not to be okay. I'm here to support you through everything.",
-                    "Do you need a hug? come here."
+                    "Do you need a hug? come here.",
+                    "Remember you're stuck with me forever so your problems are mine too, don't be afraid to share the load."
+                    
           ],
           motivation: [
                     "You can do anything you put your mind to my girl, the fact you can deal with me everyday is testament to how resilient you are.",
                     "You are the smartest, strongest and most capable woman in the world!",
-                    "I am sending you virtual kisses and butt slaps for motivation, you got this babygirl!"
+                    "I am sending you virtual kisses and butt slaps for motivation, you got this babygirl!",
+                    "You're the smartest girl in the world my love there's nothing you can't do that you set your mind to."
           ]
 };
